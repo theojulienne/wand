ffcda039ba5fbebe40fdf26b172f4706e7061989
Theo Julienne <theo.julienne@gmail.com>
2010-04-29 01:33:30 +0000

    import

---
+++ wand/jwand/wand/TreeTest.java
@@ -0,0 +1,9 @@
+package wand;
+
+public class TreeTest {
+	public static void main( String[] args ) {
+		ProgramNode node = new ProgramNode();
+		
+		System.out.println( "Hello, World!" );
+	}
+}
---
+++ wand/jwand/wand/nodes/NodeFactory.java
@@ -0,0 +1,5 @@
+package wand;
+
+public class NodeFactory {
+	
+}
