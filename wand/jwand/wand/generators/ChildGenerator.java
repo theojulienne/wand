0e7cd9d703efbb11e8b326809600413d36645e3a
Theo Julienne <theo.julienne@gmail.com>
2010-06-04 14:44:20 +0000

    missing file

---
+++ wand/jwand/wand/generators/ChildGenerator.java
@@ -0,0 +1,22 @@
+package wand.generators;
+
+import wand.core.WandNode;
+import wand.parser.*;
+
+import java.io.PrintStream;
+
+public class ChildGenerator implements Generator {
+    public static void generateChildren( WandNode node, PrintStream out ) {
+        int numChildren = node.jjtGetNumChildren( );
+		
+		for ( int i = 0; i < numChildren; i++ ) {
+			WandNode childNode = (WandNode)node.jjtGetChild(i);
+			
+			childNode.generateToStream( out );
+		}
+    }
+    
+    public void generateNode( WandNode node, PrintStream out ) {
+		generateChildren( node, out );
+	}
+}
