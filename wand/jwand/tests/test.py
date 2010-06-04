0d7278bb6f48e8200ff24d4c1b419684e3909cac
Theo Julienne <theo.julienne@gmail.com>
2010-06-04 14:31:19 +0000

    more test code (still doesn't execute), plus another test

--- wand/jwand/tests/test.py
+++ wand/jwand/tests/test.py
@@ -3,9 +3,13 @@ import os
 from terminal import TerminalController
 term = TerminalController()
 
+PASS_STATS = {}
+
 def run_test( test_type, path, files ):
 	# test is in format code-result eg "core-pass"
 	version_code, expected_result = test_type.split( '-' )
+	
+	return False # not implemented
 
 def run_tests( ):
 	for root, dirs, files in os.walk( '.' ):
@@ -29,14 +33,28 @@ def run_tests( ):
 		result = run_test( test_type, root, files )
 		
 		description = '[????]'
+		stats_code = 'unknown'
 		
 		if result:
 			description = term.GREEN + '[PASS]' + term.NORMAL
+			stats_code = 'pass'
 		else:
 			description = term.RED + '[FAIL]' + term.NORMAL
+			stats_code = 'fail'
 		
 		print ' %s  type=%-15s feature=%-15s name=%-15s' % \
 				(description, test_type, test_feature, test_name)
+		
+		if test_type not in PASS_STATS:
+			PASS_STATS[test_type] = {
+				'pass': 0,
+				'fail': 0,
+				'total': 0
+			}
+		
+		PASS_STATS[test_type]['total'] += 1
+		PASS_STATS[test_type][stats_code] += 1
+		
 
 if __name__ == '__main__':
 	print 'Running Wand tests...'
@@ -45,4 +63,19 @@ if __name__ == '__main__':
 	run_tests( )
 	
 	print
-	print 'All tests passed. You are AWESOME!'
+	print 'Results:'
+	for test_type in PASS_STATS:
+		stats = PASS_STATS[test_type]
+		num_tests = stats['total']
+		num_passed = stats['pass']
+		num_failed = stats['fail']
+		perc_passed = num_passed * 100.0 / num_tests
+		perc_failed = num_failed * 100.0 / num_tests
+		
+		print ('%15s (%3d tests): ' + \
+			term.GREEN + '%3d passed (%3d%%)' + term.NORMAL + \
+			'   ' + \
+			term.RED + '%3d failed (%3d%%)' + term.NORMAL + 
+			'') % \
+			(test_type, num_tests, num_passed, perc_passed, num_failed, perc_failed)
+	
